bc337c5dd57e977806c790a30ae40984121682d1
Al Ahmar57 <etcnicom@gmail.com>
2023-08-19 18:53:06 +0200

    ajout d'un router

--- public/main.js
+++ public/main.js
@@ -15,29 +15,3 @@ document.querySelector('#app').innerHTML = `
 `
 const div = document.querySelectorAll('#div div'); 
 setDebut(div, resulat)
-
-
-// const chronos = new Promise((resolve, reject) => {
-//   const interval = setInterval(()=> {
-//     console.log("ok")
-//     resolve(1)
-//   }, 1000)
-// })
-
-// console.log(chronos)
-
-// chronos.then((n) => {
-//   console.log(n)
-// })
-
-
-{/* <a href="https://vitejs.dev" target="_blank">
-<img src="${viteLogo}" class="logo" alt="Vite logo" />
-</a>
-<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-<img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-</a>
-<h1>Hello Vite!</h1>
-<div class="card">
-<button id="counter" type="button"></button>
-</div> */}
