39c5534879fe878dc459c2af70719ce04c901426
Al Ahmar57 <etcnicom@gmail.com>
2023-09-05 06:33:04 +0200

    ajoute tableau calcule

---
+++ scoring/tableauCalcule.js
@@ -0,0 +1,10 @@
+const tableauCalcule = []
+
+
+export function getTableauCalcule(){
+    return tableauCalcule
+}
+
+export function addTableauCalcule(calcule){
+    tableauCalcule.push(calcule)
+}
