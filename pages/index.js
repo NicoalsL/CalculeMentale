19c4c9ca0b9633e87f1375a3cf7b6a3e051e44f7
NicolasL <nicolas.lebon2@icloud.com>
2023-09-16 07:34:31 +0200

    ajoute coeur image

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ const indexPage = () => {
   const content = document.getElementById('content');
   content.innerHTML = `
     <h1>jeux</h1>
-    <button class="neubrutalism"><a class="buttonA" href="/jeu" onclick="navigateTo('/jeu'); return false;">Jouez !</a></button>
+    
+    <button class="neubrutalism"><a class="buttonA" href="/jeu" onclick="navigateTo('/jeu'); return false;">Jouez Maintenant</a></button>
     `;
 };
 
--- scoring/vie.js
+++ scoring/vie.js
@@ -16,12 +16,31 @@ export function ajoutVie(){
     vie++;
 }
 
-export function afficherVie(div){
-    const afiVie = document.createElement('h1')
-    div[0].appendChild(afiVie)
-    afiVie.classList = "classVie"
-    afiVie.innerHTML = `Vie : ${getVie()}`
-}
+// export function afficherVie(div){
+//     const afiVie = document.createElement('h1')
+//     div[0].appendChild(afiVie)
+//     afiVie.classList = "classVie"
+//     afiVie.innerHTML = `Vie : ${getVie()}`
+// }
+// Créez une fonction pour générer la chaîne HTML des cœurs
+function genererCoeurs(nombreDeVies) {
+    let coeursHTML = '';
+    for (let i = 0; i < nombreDeVies; i++) {
+      coeursHTML += '<img src="/assets/coeur.png" height="50px">';
+    }
+    return coeursHTML;
+  }
+  
+  // Modifiez la fonction afficherVie pour afficher les cœurs
+  export function afficherVie(div) {
+    const afiVie = document.createElement('div'); // Utilisez une balise div pour contenir les cœurs
+    div[0].appendChild(afiVie);
+    afiVie.classList = "classVie";
+    const nombreDeVies = getVie();
+    const coeursHTML = genererCoeurs(nombreDeVies);
+    afiVie.innerHTML = `${coeursHTML}`;
+  }
+  
 
 export function supprimerVie(div){
     const afiVie = document.querySelector(".classVie")
